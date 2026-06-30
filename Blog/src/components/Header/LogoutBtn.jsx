02ffee0b683c89bf90d020564aabaea84fa4c6bd
sanikasharma07 <vanshikabuilddevelopers@gmail.com>
2026-07-01 01:08:06 +0530

    last push

--- Blog/src/components/Header/LogoutBtn.jsx
+++ Blog/src/components/Header/LogoutBtn.jsx
@@ -2,17 +2,26 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth";
 import { logout } from "../../store/authSlice";
+
 function LogoutBtn(){
-    const dispatch=useDispatch()
-    const logoutHandler=()=>{
-        authService.logout().then(()=>{
-            dispatch(logout())
+    const dispatch = useDispatch()
+    
+    const logoutHandler = () => {
+        authService.logout().then(() => {
+            dispatch(logout()) // This updates your Redux state[cite: 2]
+            
+            // This forces a hard browser reload and sends you to the home page, 
+            // completely wiping any leftover "ghost" React memory!
+            window.location.href = "/" 
         })
     }
+    
     return(
         <button
         onClick={logoutHandler}
-        className="inline-bock px-3 md:px-6  py-2 duration-200 hover:bg-blue-100 rounded-full">Logout</button>
+        className="inline-bock px-3 md:px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+            Logout
+        </button>
     )
 }
 export default LogoutBtn
